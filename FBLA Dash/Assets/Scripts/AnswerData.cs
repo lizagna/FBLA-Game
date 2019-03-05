d8c0a2fd09dd58c3fce0ad9048ef22a71cbcfa11
Tony Luu <tony.luu@eogresources.com>
2019-03-05 14:07:58 -0600

    Added class summary

--- FBLA Dash/Assets/Scripts/AnswerData.cs
+++ FBLA Dash/Assets/Scripts/AnswerData.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 
+/// Answer data metadata
 /// </summary>
 [System.Serializable]
-public class AnswerData 
+public class AnswerData
 {
     public string answerText;
     public bool isCorrect;
-    
 }
