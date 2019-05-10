32005c36085dcb41321a676c2da0440e30e46a1a
lizagna <lizagna@users.noreply.github.com>
2019-05-10 15:48:42 -0500

    added 2nd character and fixed health

---
+++ Pixel Panic/Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour {
+     
+    public GameObject pauseMenuDisplay;
+
+    void Start() {
+        pauseMenuDisplay.gameObject.SetActive(false);
+    }
+
+    public void Pause() {
+        pauseMenuDisplay.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume() {
+        pauseMenuDisplay.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Options() {
+        
+    }
+}
